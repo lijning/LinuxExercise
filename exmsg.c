8b4c7b146ad905fdaf69cdf2f22ed1b058ea20a3
ljn <sos37809@yeah.net>
2018-06-28 09:18:25 +0800

    ex about msg

--- exmsg.c
+++ exmsg.c
@@ -16,17 +16,17 @@ int main()
     {
         close(fd12[0]);
         close(fd21[1]);
-        /*pid = fork();
+        pid = fork();
         if (pid == 0)
         { //p3
             printf("I am child process p3 \n");
-            execl("ls", "-Rl", NULL);
             printf("p3 process PID %d \n", getpid());
+            execlp("ls","ls","-Rl", NULL);
             close(fd12[1]);
             close(fd21[0]);
-			return 0;
+		return 0;
         }
-        else*/
+        else
         { //子进程p1执行代码
             printf("p1 process PID %d \n", getpid());
             write(fd12[1], p1p2, sizeof(p1p2));
